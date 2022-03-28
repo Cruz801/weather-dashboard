c796de2bbbd3cc6558f38752b47dca49edd6f219
Steven Cruz <cruz801@hotmail.com>
2022-03-27 23:38:42 -0600

    functioning

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 // moment(new Date(1648324800*1000)).format('dddd, MMMM Do YYYY')
 // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
+
+var pastHistory = JSON.parse(localStorage.getItem('pastHistory')) || [];
+
+
 $(document).ready(function () {
     var baseUrl = 'https://api.openweathermap.org/data/2.5'
+
     function getWeather(city) {
         fetch(`${baseUrl}/weather?q=${city}&appid=4fcd8b1fe7db2deebbe0c2f224b0d864`)
             .then(response => response.json())
             .then(data => {
                 var lat = data.coord.lat
                 var lon = data.coord.lon
-                getOneCall(lat, lon, city)
+                var cityName = data.name
+                getOneCall(lat, lon, cityName)
+                putCityIntoStorage(cityName);
             })
     }
 
@@ -18,17 +25,27 @@ $(document).ready(function () {
         fetch(`${baseUrl}/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely,alerts&appid=4fcd8b1fe7db2deebbe0c2f224b0d864`)
             .then(response => response.json())
             .then(data => {
-                displayCurrent(data.current,city)
+                displayCurrent(data.current, city)
                 forecast(data.daily)
             })
     }
 
-    function displayCurrent(weather,city) {
+    function displayCurrent(weather, city) {
+        var day = weather
         var iconurl = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
         console.log('current', weather)
         $('.cityName').text(city)
         $('.currentDate').text(moment().format('dddd, MMMM Do YYYY'))
-        $('.weatherIcon').attr('src',iconurl)
+        $('.weatherIcon').attr('src', iconurl)
+        $('.temp').empty()
+        $('.humidity').empty()
+        $('.wind').empty()
+        $('.uv').empty()
+      
+        $('.temp').append(` ${weather.temp}°`)
+        $('.humidity').append(` ${weather.humidity}`)
+        $('.wind').append(` ${weather.wind_speed}`)
+        $('.uv').append(` ${weather.uvi}`)
     }
 
     function forecast(weather) {
@@ -38,32 +55,53 @@ $(document).ready(function () {
             var day = weather[i]
             $('.cards').append(`<div class="card">
             <div class="card-body">
-                <h3 class="card-title">${moment(new Date(day.dt*1000)).format('dddd')}</h3>
+                <h3 class="card-title">${moment(new Date(day.dt * 1000)).format('dddd')}</h3>
                 <img src="http://openweathermap.org/img/w/${day.weather[0].icon}.png">
-                <p class="card.text">Temp: ${day.temp.max}</p>
-                <p class="card.text">Wind: ${day.wind_speed}</p>
-                <p class="card.text">Humidity: ${day.humidity}</p>
+                <p class="card.text mr-3">Temp: ${day.temp.max}</p>
+                <p class="card.text mr-3">Wind: ${day.wind_speed}</p>
+                <p class="card.text mr-3">Humidity: ${day.humidity}</p>
             </div>
         </div>`)
         }
     }
 
-function citySearch() {
-    var cityName= $('.searchInput').val()
-    if (cityName){
-        getWeather(cityName)
-    } else {
-        alert('Please enter a city');
+    function citySearch() {
+        var cityName = $('.searchInput').val()
+        if (cityName) {
+            getWeather(cityName)
+        } else {
+            alert('Please enter a city');
+        }
+        // save city to localstorage new function
     }
-    // save city to localstorage new function
 
-}
 
-function getCityFromStorage() {
 
-}
 
-    
-$('.searchBtn').click(citySearch);
-getWeather('Salt Lake City')
+    function putCityIntoStorage(cityName) {
+        if (pastHistory.includes(cityName)) return;
+        $('.prevSearch').empty();
+        pastHistory.push(cityName)
+        localStorage.setItem('pastHistory', JSON.stringify(pastHistory))
+        createButtons()
+    }
+
+    function createButtons() {
+        for (var i = 0; i < pastHistory.length; i++) {
+            var city = pastHistory[i]
+            var buttonCity = $('<button class="btnSearch">').text(city)
+            $('.prevSearch').append(buttonCity)
+            
+        }
+    }
+
+
+    $('.searchBtn').click(citySearch);
+    $('.prevSearch').on('click','button', function (event) {
+        var city = event.target.innerText
+    getWeather(city);
+    });
+    getWeather('Salt Lake City')
+    createButtons();
+
 });
